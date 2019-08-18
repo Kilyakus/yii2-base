e59e6c29be363ee0dc91479ad3dc6fcd5d76a1ad
Kilyakus <aleksandr.kilyakov@gmail.com>
2019-08-19 00:30:34 +0500

    initial commit

--- src/BaseAsset.php
+++ src/BaseAsset.php
@@ -5,11 +5,19 @@ class BaseAsset extends \yii\web\AssetBundle
 {
     public function init()
     {
-        $this->sourcePath = __DIR__;
+        $this->sourcePath = __DIR__ . '/assets';
     }
+    public $js = [
+        'jquery/dist/jquery.js',
+        'bootstrap/dist/js/bootstrap.min.js',
+    ];
+
     public $depends = [
         'kilyakus\fonts\FontAsset',
-        'kilyakus\toastr\ToastrAsset'
+        'kilyakus\toastr\ToastrAsset',
+        'yii\web\JqueryAsset',
+        'yii\bootstrap\BootstrapAsset',
+        'yii\bootstrap\BootstrapPluginAsset',
     ];
     public $jsOptions = array(
         'position' => \yii\web\View::POS_HEAD
